55ce3238446eb69f525a927a1e2fdeff710da3d8
飞 <153630093+cbofjou@users.noreply.github.com>
2023-12-26 21:43:20 +0800

    Update and rename gaojingduchengfa.c to 文件名gaojingduchengfa.c

--- 文件名gaojingduchengfa.c
+++ 文件名gaojingduchengfa.c
@@ -1,3 +1,6 @@
+高精度乘法
+
+
 #include<stdio.h>
 #include<math.h>
 #include<string.h>
@@ -13,7 +16,7 @@ int main()
     n = strlen(arr);
     int a = 0;
     int yu = 0;
-    for (int i = m - 1;i >= 0;i--)
+    for (int i = m - 1;i >= 0;i--)                                   //此处将对应位上数相乘并相加，先不进位
     {
         a = m - i - 1;
         for (int j = n - 1; j >= 0;j--)
@@ -22,7 +25,7 @@ int main()
             a++;
         }
     }
-    for (int i = 0;i <= a;i++)
+    for (int i = 0;i <= a;i++)                                       //此处才开始进位，这样简洁，可读性强
     {
         count[i + 1] = count[i + 1] + count[i] / 10;
         count[i] = count[i] % 10;
